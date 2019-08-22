083dbfe73279516eab157ed5f7b3648773b206da
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-22 01:18:49 -0300

    Adiciona configuração do ESLint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -11,7 +11,6 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
-    1;
   }
 
   routes() {
