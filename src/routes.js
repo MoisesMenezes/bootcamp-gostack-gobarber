43c0ab190c4dcae0ad8e1b36c8979c89593fb126
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-23 01:13:45 -0300

    Adiciona Session do usuario

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionControllers from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControllers.store);
 
 export default routes;
