cd52cc97354b0abe31c81acf83c0094ce4e36ff9
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-23 00:24:22 -0300

    Adiciona controller do Usuario

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,24 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const emailExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (emailExists) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
