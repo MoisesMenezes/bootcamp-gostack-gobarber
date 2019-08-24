1e057672c680401b61bccdf50ab2d8eee9301118
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-23 23:49:40 -0300

    Adiciona funcionalidade de Update do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,9 +21,32 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const emailExists = await User.findOne({
+        where: { email },
+      });
+
+      if (emailExists) {
+        return res.status(400).json({ error: 'Email already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
