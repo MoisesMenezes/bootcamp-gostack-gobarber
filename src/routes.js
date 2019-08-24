aff60e3cf95654aa3315a1e26b9f542d045f5570
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-23 23:06:49 -0300

    Adiciona Middleware de autentificação do usuário

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionControllers from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionControllers.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
