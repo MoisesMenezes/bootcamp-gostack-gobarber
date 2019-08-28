40766058a5c978e64b1a75081cd27449118b6e6c
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-28 14:41:00 -0300

    Adiciona Model e controller do File

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionControllers from './app/controllers/SessionController';
-
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -17,8 +17,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
