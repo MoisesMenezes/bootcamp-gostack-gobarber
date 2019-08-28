5c5254acd949c299ee9b1a36a9b35cf3d7c8714e
Moises Menezes <moisesmenezes90@gmail.com>
2019-08-28 00:32:19 -0300

    Adicona lib Multer com upload de arquivo

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionControllers from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionControllers.store);
@@ -12,5 +16,9 @@ routes.post('/sessions', SessionControllers.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
