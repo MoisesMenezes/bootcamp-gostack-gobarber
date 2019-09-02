440e39a9ccb34d08c022b92cff1927b58aa4d5ec
Moises Menezes <moisesmenezes90@gmail.com>
2019-09-01 22:50:08 -0300

    Adiciona envio de email para o cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c5287d8517a314',
+    pass: '6e9ea18905f34a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
