67a2301042bbee39643d59d972991907ae404a0d
Moises Menezes <moisesmenezes90@gmail.com>
2019-09-03 17:11:59 -0300

    Criar variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'c5287d8517a314',
-    pass: '6e9ea18905f34a',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://2443ca1bb1a24cde979f16c5c64c70a6@sentry.io/1550580',
+  dsn: process.env.SENTRY_DSN,
 };
